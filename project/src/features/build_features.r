ce840db94c124f4d44ff8e422a08b0680dce3708
dicicch <hsdicicco@gmail.com>
2020-03-25 20:04:33 -0400

    Nearly finished preprocessing

--- project/src/features/build_features.r
+++ project/src/features/build_features.r
@@ -16,7 +16,7 @@ for (file in filelist) {
   assign(strsplit(tail(strsplit(file, "/")[[1]], n=1), # xxx.csv
                   ".csv")[[1]], # xxx
          fread(paste0("project/volume/data/raw/",
-                      file)
+                      file) # xxx = read data
          )
   )
 }
@@ -26,14 +26,11 @@ rm("file", "filelist")
 strsplit(example_sub$id, "_")
 unlist(strsplit(example_sub$id, "_"))
   
-examp_sub <- data.table(matrix(unlist(strsplit(example_sub$id, "_")), ncol = 4, byrow = TRUE))
-rm(example_sub)
+examp_sub <- data.table(matrix(as.numeric(unlist(strsplit(example_sub$id, "_"))), ncol = 4, byrow = TRUE))
 head(examp_sub, n=5)
 setnames(examp_sub,
          c("V1", "V2", "V3", "V4"),
          c("Season", "DayNum", "Team1", "Team2"))
-examp_sub[,Season := as.integer(Season)]
-examp_sub[,DayNum := as.integer(DayNum)]
 head(examp_sub, n=5)
 examp_sub[, result := 0.5]
 head(examp_sub, n=5)
@@ -46,12 +43,15 @@ setnames(train, c("WTeamID","LTeamID"), c("Team1","Team2"))
 head(train, n=5)
 
 train$result <- 1
+train = train[, c(3,1,2,4,5)] # reorder columns so season is first
+head(train, n=5)
 
-# make master data file
-
+# make master data
 master <- rbind(train,examp_sub)
-master$team_1 <- as.character(master$team_1)
-master$team_2 <- as.character(master$team_2)
+head(master)
+master[,Team1:=as.character(Team1)]
+master[,Team2:=as.character(Team2)]
+head(master)
 
 head(massey, n=5)
 
@@ -59,40 +59,45 @@ massey$DayNum <- massey$RankingDayNum + 1
 head(massey, n=5)
 pom_ranks <- massey[SystemName == "POM", .(Season, DayNum, TeamID, OrdinalRank)]
 head(pom_ranks, n=5)
-setnames(pom_ranks, "TeamID", "team_1")
+setnames(pom_ranks, "TeamID", "Team1")
+head(pom_ranks)
+pom_ranks[,Team1:=as.character(Team1)]
+head(pom_ranks)
 
-pom_ranks
-pom_ranks$team_1 <- as.character(pom_ranks$team_1)
+pom_ranks = pom_ranks[,c(1,3,2,4)]
 
-setkey(master, Season,team_1, DayNum)
-setkey(pom_ranks, Season, team_1, DayNum)
+setkey(master, Season, Team1, DayNum)
+setkey(pom_ranks, Season, Team1, DayNum)
 
 head(master)
 head(pom_ranks)
 
 master <- pom_ranks[master, roll = T]
-master
+head(master)
 setnames(master,"OrdinalRank", "team_1_POM")
 
-setnames(pom_ranks,"team_1","team_2")
-setkey(master,Season,team_2,DayNum)
-setkey(pom_ranks,Season,team_2,DayNum)
+setnames(pom_ranks,"Team1","Team2")
+setkey(master, Season, Team2, DayNum)
+setkey(pom_ranks, Season, Team2, DayNum)
 
-master <- pom_ranks[master,roll = T]
+master <- pom_ranks[master, roll = T]
+head(master)
 
-setnames(master,"OrdinalRank","team_2_POM")
+setnames(master, "OrdinalRank", "team_2_POM")
 
 master
 master[, POM_dif := team_2_POM - team_1_POM]
 master
+master = na.omit(master, cols=c("team_1_POM", "team_2_POM", "POM_dif"), invert=FALSE)
+master
+master[is.na(POM_dif)==T, .N]
 master <- master[order(Season, DayNum)]
+master
 
-master <- master[,.(team_1,team_2, POM_dif, result)]
+master <- master[, .(Season, Team1, Team2, POM_dif, result)]
+master
 
-
-master <- master[!is.na(master$POM_dif)]
-
-examp_sub <- master[result == 0.5]
+examp_sub <- master[result==0.5]
 train <- master[result == 1]
 
 rand_idx <- sample(1:nrow(train), nrow(train) * 0.5)
